35277823c80bf7bbba002081c413c6bbd45e2fe0
Adweeb252 <aditya.tomar.ug21@nsut.ac.in>
2023-01-14 02:11:42 +0530

    2nd commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,45 @@
-document.querySelector(".set").addEventListener("click", handleclick);
+document.querySelector(".w").addEventListener("click", handleclick1);
+document.querySelector(".a").addEventListener("click", handleclick2);
+document.querySelector(".s").addEventListener("click", handleclick3);
+document.querySelector(".d").addEventListener("click", handleclick4);
+document.querySelector(".j").addEventListener("click", handleclick5);
+document.querySelector(".k").addEventListener("click", handleclick6);
+document.querySelector(".l").addEventListener("click", handleclick7);
 
-function handleclick()
+function handleclick1()
 {
-    alert("i am clicked");
+    var audio1 = new Audio('sounds/tom-1.mp3');
+    audio1.play();
+
+    this.style.color= "red";
+}
+function handleclick2()
+{
+    var audio1 = new Audio('sounds/tom-2.mp3');
+    audio1.play();
+}
+function handleclick3()
+{
+    var audio1 = new Audio('sounds/tom-3.mp3');
+    audio1.play();
+}
+function handleclick4()
+{
+    var audio1 = new Audio('sounds/tom-4.mp3');
+    audio1.play();
+}
+function handleclick5()
+{
+    var audio1 = new Audio('sounds/crash.mp3');
+    audio1.play();
+}
+function handleclick6()
+{
+    var audio1 = new Audio('sounds/kick-bass.mp3');
+    audio1.play();
+}
+function handleclick7()
+{
+    var audio1 = new Audio('sounds/snare.mp3');
+    audio1.play();
 }
